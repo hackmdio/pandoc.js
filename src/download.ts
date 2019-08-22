ee80c36e426e705578489c73fc7d3e91ca1c2573
Yukai Huang <yukaihuangtw@gmail.com>
2019-08-22 17:51:01 +0800

    Extract and move binary to pandoc download folder

--- src/download.ts
+++ src/download.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs-extra'
 import * as path from 'path'
 import got from 'got'
 import ProgressBar from 'progress'
+import extract from 'extract-zip'
+import glob from 'glob'
 
 type supportedPlastform = 'darwin' | 'linux' | 'win32'
 
@@ -16,25 +18,56 @@ const filterAssets = asset => {
   return asset.name.indexOf(archStr) >= 0 && asset.name.indexOf('.zip') >= 0;
 }
 
+const extractP = (a, b) => new Promise((resolve, reject) => {
+  extract(a, b, function (err) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve()
+    }
+  })
+})
+
+const globP = (a, b = {}) => new Promise((resolve, reject) => {
+  glob(a, b, function (err, files) {
+    if (err) {
+      reject(err)
+    } else {
+      resolve(files)
+    }
+  })
+})
+
 async function run () {
   const { body: release } = await got('https://api.github.com/repos/jgm/pandoc/releases/latest', { json: true })
 
-  const downloadDir = path.join(__dirname, `../.pandoc-local/${release.tag_name}`)
+  const donwloadRoot = path.join(__dirname, '../.pandoc-local')
+  const downloadDir = path.join(donwloadRoot, release.tag_name)
   fs.ensureDirSync(downloadDir)
 
   const asset = release.assets.find(filterAssets)
   if (asset) {
-    var bar = new ProgressBar('  downloading [:bar] :percent :etas', { total: 15 })
+    const bar = new ProgressBar('  downloading [:bar] :percent :etas', { total: 15 })
+    const zipPath = `${downloadDir}/${asset.name}`
 
-    return new Promise((resolve, reject) => {
-      got
-        .stream(asset.browser_download_url)
-        .on('downloadProgress', progress => {
-          bar.update(progress.percent)
-        })
-        .pipe(fs.createWriteStream(`${downloadDir}/${asset.name}`))
-        .on('finish', resolve)
-        .on('error', reject)
+    if (!fs.existsSync(zipPath)) {
+      await new Promise((resolve, reject) => {
+        got
+          .stream(asset.browser_download_url)
+          .on('downloadProgress', progress => {
+            bar.update(progress.percent)
+          })
+          .pipe(fs.createWriteStream(zipPath))
+          .on('finish', resolve)
+          .on('error', reject)
+      })
+    }
+
+    await extractP(zipPath, { dir: downloadDir })
+
+    const files = await globP(`${downloadDir}/**/{pandoc,pandoc-citeproc}`)
+    ;(files as string[]).forEach(f => {
+      fs.copyFileSync(f, path.join(donwloadRoot, path.basename(f)))
     })
   }
 }
